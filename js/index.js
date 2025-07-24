dac4fb2c54d243758beeadcf7f64fe416c9bfd3f
rrahman <rashidur92@hotmail.com>
2025-07-24 17:17:13 +0100

    added tabs for projects with background images, initial css. still need to play around with colors and padding etc.

--- js/index.js
+++ js/index.js
@@ -3,3 +3,29 @@
 // let url = window.location.pathname
 
 // root.innerText = "this is the " + url +" page as changed by javascript"
+
+function openProject(event){
+    /* don't necessarily like this way of getting project name from the id
+    of the button which has to match the id of the corresponding contents but
+    with 'tab_' before it for this to work. may be better way of doing this.
+    */
+    let projectName = event.currentTarget.id.split("_")[1]
+    let tabcontents = document.getElementsByClassName("tabcontent")
+    let tablinks = document.getElementsByClassName("tablinks")
+
+    //hide all tabcontents
+    Array.from(tabcontents).forEach(element => element.style.display = "none");
+
+    //remove active from all tablinks
+    Array.from(tablinks).forEach(element => element.className = element.className.replace(" active", ""))
+
+    //put active back on actually selective tablink
+    document.getElementById(projectName).style.display = "block";
+    event.currentTarget.className += " active"
+}
+
+let tablinks = document.getElementsByClassName("tablinks")
+//annoying workaround because HTMLCollectionOf is not actually an array so can't foreach. could do classic for loop instead of this.
+Array.from(tablinks).forEach(element => {
+    element.addEventListener("click", openProject)
+})
