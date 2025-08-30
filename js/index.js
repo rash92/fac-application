fa1acf358778dd1e771c06310f85976c87410136
rash92 <rashidur92@hotmail.com>
2025-08-30 18:10:36 +0100

    removed commented out unused code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-// let root = document.getElementById("root")
-
-// let url = window.location.pathname
-
-// root.innerText = "this is the " + url +" page as changed by javascript"
-
 function openProject(event){
     /* don't necessarily like this way of getting project name from the id
     of the button which has to match the id of the corresponding contents but
@@ -16,16 +10,15 @@ function openProject(event){
     //hide all tabcontents
     Array.from(tabcontents).forEach(element => element.style.display = "none");
 
-    //remove active from all tab
+    //remove active from all tabs
     Array.from(tab).forEach(element => element.className = element.className.replace(" active", ""))
 
-    //put active back on actually selective tablink
+    //put active back on actually selected tablink
     document.getElementById(projectName).style.display = "block";
     event.currentTarget.className += " active"
 }
 
 let tab = document.getElementsByClassName("tab")
-//annoying workaround because HTMLCollectionOf is not actually an array so can't foreach. could do classic for loop instead of this.
 Array.from(tab).forEach(element => {
     element.addEventListener("click", openProject)
 })
